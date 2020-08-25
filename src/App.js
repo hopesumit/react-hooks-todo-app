8b18a69112c53b924f69ded274b07491c4eaed6e
Sumit Kashyap <sumit@digithinkit.com>
2020-08-25 16:28:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer, useEffect, useRef } from "react";
+import "./App.css";
+
+const initialState = [];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      return [...state, { name: action.name }];
+    case "remove":
+      return state.filter((item, index) => index !== action.index);
+    
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [tasks, dispatch] = useReducer(reducer, initialState);
+  
+
+  const inputRef = useRef(null);
+
+  useEffect(() => inputRef.current.focus());
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: "add",
+      name: inputRef.current.value,
+    });
+    inputRef.current.value = "";
+  }
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Todos</h1>
+        <form className="to-do-form" onSubmit={handleSubmit}>
+          <input type="text" placeholder="Add new task" ref={inputRef} />
+          <button className="arrow-buton">
+            <i class="fa fa-arrow-right" aria-hidden="true"></i>
+          </button>
+        </form>
+
+      <div className="list">
+        <ul>
+          {tasks.map((task, index) => (
+            <li className="list" key={index}>
+              {task.name}
+              <button onClick={() => dispatch({ type: "remove", index })}>
+              <i class="fa fa-trash" aria-hidden="true"></i>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import renderer from 'react-test-renderer';
+
+test("todo snapshot test", ()=> {
+    const component = renderer.create(
+        <App />
+    );
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+})
