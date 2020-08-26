70772463a5f5b199b96c49bb2e46a0502ad5948c
Sumit Kashyap <sumit@digithinkit.com>
2020-08-26 14:32:56 +0530

    added toggle functionality for task and follow redux convention

--- src/App.js
+++ src/App.js
@@ -1,60 +1,82 @@
-import React, { useState, useReducer, useEffect, useRef } from "react";
+import React, { useState, useReducer } from "react";
 import "./App.css";
 
 const initialState = [];
 
-const reducer = (state, action) => {
+const reducer = (todos, action) => {
   switch (action.type) {
     case "add":
-      return [...state, { name: action.name }];
+      return [...todos, newTodo(action.payload.name)];
     case "remove":
-      return state.filter((item, index) => index !== action.index);
-    
+      return todos.filter((todo) => todo.id !== action.payload.id);
+    case "toggle":
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
     default:
-      return state;
+      return todos;
   }
 };
 
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
+
 function App() {
-  const [tasks, dispatch] = useReducer(reducer, initialState);
-  
-
-  const inputRef = useRef(null);
-
-  useEffect(() => inputRef.current.focus());
+  const [todos, dispatch] = useReducer(reducer, initialState);
+  const [name, setName] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
-
     dispatch({
       type: "add",
-      name: inputRef.current.value,
+      payload: { name: name },
     });
-    inputRef.current.value = "";
+    setName("");
   }
- 
 
   return (
     <div className="App">
-    <h1>Todos</h1>
-        <form className="to-do-form" onSubmit={handleSubmit}>
-          <input type="text" placeholder="Add new task" ref={inputRef} />
-          <button className="arrow-buton">
-            <i class="fa fa-arrow-right" aria-hidden="true"></i>
-          </button>
-        </form>
+      <h1>Todos</h1>
+      <form className="to-do-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add new task"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button className="arrow-buton">
+          <i class="fa fa-arrow-right" aria-hidden="true"></i>
+        </button>
+      </form>
+      <div className="todos">All({todos.length})</div>
 
       <div className="list">
-        <ul>
-          {tasks.map((task, index) => (
-            <li className="list" key={index}>
-              {task.name}
-              <button onClick={() => dispatch({ type: "remove", index })}>
+        {todos.map((todo) => (
+          <div className="list" key={todo.id}>
+            <span style={{ color: todo.complete ? "#AAA" : "#000" }}>
+              {todo.name}
+            </span>
+            <button
+              onClick={() =>
+                dispatch({ type: "remove", payload: { id: todo.id } })
+              }
+            >
               <i class="fa fa-trash" aria-hidden="true"></i>
-              </button>
-            </li>
-          ))}
-        </ul>
+            </button>
+            <button
+              className="toggle-button"
+              onClick={() =>
+                dispatch({ type: "toggle", payload: { id: todo.id } })
+              }
+            >
+              Complete
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
